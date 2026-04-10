3e4bca485796db828e18bee79241d0b3d63dacbe
Juan Fer <jfmoreno10@espe.edu.ec>
2026-04-09 21:15:59 -0500

    Version 2

--- ejercicio.c
+++ ejercicio.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main(){
-    float num_1;
-    float num_2;
+    float n_1;
+    float n_2;
     float promedio = 0;
 
 do{
     printf ("Ingrese la primera nota");
-    scanf ("%f", &num_1);
-}while (num_1<0 || num_1>20);
+    scanf ("%f", &n_1);
+}while (n_1<0 || n_1>20);
 
 do {
     printf ("Ingrese la segunda nota");
-    scanf ("%f", &num_2);
-}while (num_2<0|| num_2>20);
-    promedio= (num_1+num_2)/2;
+    scanf ("%f", &n_2);
+}while (n_2<0|| n_2>20);
+    promedio= (n_1+n_2)/2;
 
     printf ("Promedio: %.2f", promedio);
 
